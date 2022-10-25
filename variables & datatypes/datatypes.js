db9e6455b504e480b88e5a7a9bad2e74f43e247d
Argonx21 <gaurish2123ksn@gmail.com>
2022-10-25 22:05:40 +0530

    new changes

---
+++ variables & datatypes/datatypes.js
@@ -0,0 +1,18 @@
+// primitive datatypes in javascript
+
+let a = null; //null
+let b = 123.4; //number
+let c = true; //boolean
+let d = BigInt("123123"); // bigint
+let e = "Harry"; // string
+let f = Symbol("symbol in js"); // symbol
+let g = undefined; //undefined
+
+console.log(typeof a);
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+
