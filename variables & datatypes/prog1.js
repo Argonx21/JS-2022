253bdd3eaae61b19e969cefda61ebe933be08914
Argonx21 <gaurish2123ksn@gmail.com>
2022-10-25 20:57:26 +0530

    new commit

--- variables & datatypes/prog1.js
+++ variables & datatypes/prog1.js
@@ -1,1 +1,33 @@
 //variables & datatypes in JS
+
+
+// To declare variables in JS below keywords can be used
+var newVariable = "This is a string"; // Its not a good practice to use var keyword to declare a variable in JS as variable can be treated as a global variable.
+
+// Instead of var keyword use let keyword for variable declaration
+let newVar2 = "This is a new variable";
+
+// Declaring a constant variable
+const const1 = 23;
+
+
+// Variable datatypes
+
+let var1=10; // integer
+let var2=10.3; //float
+let var3="string"; //string
+let var4=true; // boolean type
+let var5={"firstname":"Gaurish","lastname":"Kauthankar"}; //object
+
+
+console.log(var1)
+console.log(var2)
+console.log(var3)
+console.log(var4)
+console.log(var5)
+
+
+
+
+
+
